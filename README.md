46881d8c5b3185dabc6c04d49974adb29c0c8e78
ryanYtan <tanyuryan@gmail.com>
2022-06-07 21:12:30 +0800

    update project

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # hyper-win2wsl-paths
-Simple plugin for Hyper.js terminal emulator to convert Windows paths to WSL paths
+Simple plugin for Hyper.js terminal emulator to convert Windows paths to WSL
+paths when drag-and-dropping into the terminal.
+
+Note that resolving of symlinks are **not** supported.
--- index.js
+++ index.js
@@ -1,18 +1,30 @@
-const isWindows = process.platform == 'win32';
-const isWindowsPath = (p) => /[A-Za-z]:\\.*/.test(p);
+const isWindowsPath = (path) => {
+    const winPathRegex = /[A-Za-z]:\\.*/;
+    return winPathRegex.test(path);
+}
+
+const toWslPath = (windowsPath) => {
+    const path = windowsPath;
+    const drive = path[0];
+    let newPath = `/mnt/${drive.toLowerCase()}/${path.slice(3)}`;
+    newPath = newPath.replace(/\\/g, "/");
+    newPath = newPath.replace(/\\\\/g, "/");
+    return newPath;
+}
 
 exports.middleware = (store) => (next) => (action) => {
+    if (process.platform != 'win32') {
+        next(action);
+        return;
+    }
+
     switch (action.type) {
-        case 'CONFIG_LOAD':
-            // for configs later
-            break;
-        case 'SESSION_USER_DATA':
+        case "SESSION_USER_DATA":
             const possiblePath = action.data;
-            if (isWindows && isWindowsPath(possiblePath)) {
-                const winPath = possiblePath;
-                const newPath = winPath
-                    .replaceAll("\\", "/")
-                    .replace("C:", "/mnt/c")
+            if (isWindowsPath(possiblePath)) {
+                const path = possiblePath;
+                const newPath = toWslPath(path);
+
                 action.effect = () => {
                     rpc.emit('data', {
                         uid: store.getState().sessions.activeUid,
@@ -21,6 +33,11 @@ exports.middleware = (store) => (next) => (action) => {
                     });
                 }
             }
-    };
+            break;
+
+        case "CONFIG":
+            break;
+    }
+
     next(action);
 };
